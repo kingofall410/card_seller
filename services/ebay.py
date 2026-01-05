9b97776309e74f38f2069745dd27a9465014c1a9
kingofall410 <dancrown@gmail.com>
2026-01-05 14:44:22 -0500

    updated chrome path2

--- services/ebay.py
+++ services/ebay.py
@@ -559,7 +559,7 @@ def scrape_with_profile(keyword_strings, limit=50, max_pages=3, days=180):
 
     with sync_playwright() as p:
         user_data_dir = "ebay_profile"
-        browser = p.chromium.launch_persistent_context(user_data_dir, headless=False, executable_path="usr/bin/google-chrome", \
+        browser = p.chromium.launch_persistent_context(user_data_dir, headless=False, executable_path="/usr/bin/google-chrome", \
             args=["--use-gl=desktop", "--ignore-gpu-blocklist", "--disable-gpu-sandbox", \
             "--enable-gpu-rasterization", "--enable-zero-copy"])
         page = browser.new_page()
