5fc6ad7dd489ec6681ec4ce7879e7b74f1e50fc1
kingofall410 <dancrown@gmail.com>
2026-01-12 13:55:49 -0500

    basic scheduler ported, now need to tweak it to call views

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,14 @@
+import os
 from django.apps import AppConfig
-
+#from services.queue import Queue
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # Start the queue when Django starts
+        from services.queue import Queue
+        if os.environ.get("RUN_MAIN") == "true":
+            self.queue = Queue(interval=10.0)
+            self.queue.start()
--- core/views/export_views.py
+++ core/views/export_views.py
@@ -1,12 +1,15 @@
 # Export-related views
 
-import json
+import time
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from services.models import Settings
 from core.models.CardSearchResult import CardSearchResult
 from services import export as export_handler
 from django.shortcuts import render, redirect, get_object_or_404
+from core.apps import CoreConfig
+from services.queue import Task
+from django.apps import apps
 
 @csrf_exempt
 def export_card(request, csr_id):
@@ -21,17 +24,29 @@ def export_card(request, csr_id):
 
 @csrf_exempt
 def list_card(request, csr_id):
+    
     settings = Settings.get_default()
     publish = request.GET.get('publish', True)
     group_key = request.GET.get('group_key', None)
-    print("pub, group_key", publish, group_key)
+    publish_dt = request.GET.get('publish_dt', None)
+    print("pub, group_key", publish, group_key, publish_dt)
     
     if not csr_id or csr_id == 'undefined':
         return JsonResponse({'error': 'CSR ID is required'}, status=400)
         
     csr = CardSearchResult.objects.get(id=csr_id)
     print("csr", csr, group_key)
-    success, _, _ = export_handler.export_to_ebay([csr], publish=publish, group_key=group_key)
+
+    #if publish_dt:
+    #scheduled for the future
+    task = Task(name="send_email", time=time.time() + 10, callback=print("hello"), params={})
+
+    core_config = apps.get_app_config("core")
+    core_config.queue.add(task)
+
+    success = True
+    #else:
+    #    success, _, _ = export_handler.export_to_ebay([csr], publish=publish, group_key=group_key)
     if success: 
         return JsonResponse({"success": success}, status=200)
     if not success:
